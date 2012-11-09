5530f81b4eb092f9f318bc9084929cc7dc4d701e
Rohith Avatapally <Rohith.Avatapally@catalinamarketing.com>
2012-11-09 15:04:57 -0500

    Modified Test Class

--- src/com/example/testing/TestClass.java
+++ src/com/example/testing/TestClass.java
@@ -1,5 +1,14 @@
 package com.example.testing;
 
-public class TestClass {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class TestClass extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+	
 }
